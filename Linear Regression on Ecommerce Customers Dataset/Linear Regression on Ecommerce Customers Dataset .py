70e922f1a021714832c67dd2db988c0f1261db1c
MNOUMANKHALIQ <99765047+MNOUMANKHALIQ@users.noreply.github.com>
2023-08-18 15:07:46 +0500

    Add files via upload

---
+++ Linear Regression on Ecommerce Customers Dataset/Linear Regression on Ecommerce Customers Dataset .py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
+
+# In[2]:
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[3]:
+
+
+customers = pd.read_csv("Ecommerce Customers.csv")
+
+
+# **Check the head of customers, and check out its info() and describe() methods.**
+
+# In[4]:
+
+
+customers.head()
+
+
+# In[5]:
+
+
+customers.describe()
+
+
+# In[17]:
+
+
+customers.info()
+
+
+# In[18]:
+
+
+sns.set_palette("GnBu_d")
+sns.set_style('whitegrid')
+
+
+# ## Exploratory Data Analysis
+# 
+# **Let's explore the data!**
+# 
+# For the rest of the exercise we'll only be using the numerical data of the csv file.
+# ___
+# **Use seaborn to create a jointplot to compare the Time on Website and Yearly Amount Spent columns. Does the correlation make sense?**
+
+# In[19]:
+
+
+sns.jointplot(x='Time on Website',y='Yearly Amount Spent',data=customers)
+
+
+# ** Do the same but with the Time on App column instead. **
+
+# In[20]:
+
+
+sns.jointplot(x='Time on App',y='Yearly Amount Spent',data=customers)
+
+
+# ** Use jointplot to create a 2D hex bin plot comparing Time on App and Length of Membership.**
+
+# In[21]:
+
+
+sns.jointplot(x='Time on App',y='Length of Membership',kind="hex",data=customers)
+
+
+# **Let's explore these types of relationships across the entire data set. Use [pairplot](https://stanford.edu/~mwaskom/software/seaborn/tutorial/axis_grids.html#plotting-pairwise-relationships-with-pairgrid-and-pairplot) to recreate the plot below
+
+# In[24]:
+
+
+sns.pairplot(customers)
+
+
+# **Based off this plot what looks to be the most correlated feature with Yearly Amount Spent?**
+
+# In[ ]:
+
+
+# Length of Membership 
+
+
+# **Create a linear model plot (using seaborn's lmplot) of  Yearly Amount Spent vs. Length of Membership. **
+
+# In[13]:
+
+
+sns.lmplot(x='Length of Membership',y='Yearly Amount Spent',data=customers)
+
+
+# ## Training and Testing Data
+# 
+# Now that we've explored the data a bit, let's go ahead and split the data into training and testing sets.
+# ** Set a variable X equal to the numerical features of the customers and a variable y equal to the "Yearly Amount Spent" column. **
+
+# In[25]:
+
+
+X = customers[['Avg. Session Length','Time on App','Time on Website','Length of Membership']]
+
+
+# In[26]:
+
+
+y = customers['Yearly Amount Spent']
+
+
+# ** Use model_selection.train_test_split from sklearn to split the data into training and testing sets. Set test_size=0.3 and random_state=101**
+
+# In[27]:
+
+
+from sklearn.model_selection import train_test_split
+
+
+# In[28]:
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+
+# ## Training the Model
+# 
+# Now its time to train our model on our training data!
+# 
+# ** Import LinearRegression from sklearn.linear_model **
+
+# In[29]:
+
+
+from sklearn.linear_model import LinearRegression
+
+
+# **Create an instance of a LinearRegression() model named lm.**
+
+# In[30]:
+
+
+lm = LinearRegression()
+
+
+# ** Train/fit lm on the training data.**
+
+# In[31]:
+
+
+lm.fit(X_train,y_train)
+
+
+# **Print out the coefficients of the model**
+
+# In[32]:
+
+
+lm.coef_
+
+
+# ## Predicting Test Data
+# Now that we have fit our model, let's evaluate its performance by predicting off the test values!
+# 
+# ** Use lm.predict() to predict off the X_test set of the data.**
+
+# In[33]:
+
+
+predictions = lm.predict(X_test)
+
+
+# ** Create a scatterplot of the real test values versus the predicted values. **
+
+# In[34]:
+
+
+plt.scatter(y_test,predictions)
+plt.xlabel('Y Test')
+plt.ylabel('Predicted Y')
+
+
+# In[24]:
+
+
+from sklearn import metrics
+
+
+# ## Evaluating the Model
+# 
+# Let's evaluate our model performance by calculating the residual sum of squares and the explained variance score (R^2).
+# 
+# ** Calculate the Mean Absolute Error, Mean Squared Error, and the Root Mean Squared Error. Refer to the lecture or to Wikipedia for the formulas**
+
+# In[25]:
+
+
+print('MAE :'," ", metrics.mean_absolute_error(y_test,predictions))
+print('MSE :'," ", metrics.mean_squared_error(y_test,predictions))
+print('RMAE :'," ", np.sqrt(metrics.mean_squared_error(y_test,predictions)))
+
+
+# ## Residuals
+# 
+# we should have gotten a very good model with a good fit. Let's quickly explore the residuals to make sure everything was okay with our data. 
+# 
+# **Plot a histogram of the residuals and make sure it looks normally distributed. Use either seaborn distplot, or just plt.hist().**
+
+# In[35]:
+
+
+sns.distplot(y_test - predictions,bins=50)
+
+
+# ## Conclusion
+# We still want to figure out the answer to the original question, do we focus our efforst on mobile app or website development? Or maybe that doesn't even really matter, and Membership Time is what is really important.  Let's see if we can interpret the coefficients at all to get an idea.
+# 
+# ** Recreate the dataframe below. **
+
+# In[27]:
+
+
+coeffecients = pd.DataFrame(lm.coef_,X.columns)
+coeffecients.columns = ['Coeffecient']
+coeffecients
+
+
+# ** How can you interpret these coefficients? **
+
+# Interpreting the coefficients:
+# 
+# - Holding all other features fixed, a 1 unit increase in **Avg. Session Length** is associated with an **increase of 25.98 total dollars spent**.
+# - Holding all other features fixed, a 1 unit increase in **Time on App** is associated with an **increase of 38.59 total dollars spent**.
+# - Holding all other features fixed, a 1 unit increase in **Time on Website** is associated with an **increase of 0.19 total dollars spent**.
+# - Holding all other features fixed, a 1 unit increase in **Length of Membership** is associated with an **increase of 61.27 total dollars spent**.
